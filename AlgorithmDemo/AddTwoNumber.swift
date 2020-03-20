802e5cdeedb7868f14f9f2abcf9f58f125ea599c
王嘉宁 <wangjianing@imdada.cn>
2020-03-20 19:11:03 +0800

    add two numbers

---
+++ AlgorithmDemo/AddTwoNumber.swift
@@ -0,0 +1,86 @@
+//
+//  AddTwoNumber.swift
+//  AlgorithmDemo
+//
+//  Created by 王嘉宁 on 2020/3/20.
+//  Copyright © 2020 王嘉宁. All rights reserved.
+//
+
+import Foundation
+
+
+// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+ 
+extension Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        
+        var resultList: ListNode = ListNode(0)
+        let resultListNode: ListNode = resultList
+        var l1Node = l1
+        var l2Node = l2
+        var needCarry = false//[2,4,3] [5,6,4]
+        while l1Node != nil && l2Node != nil {
+            var newVal = l1Node!.val + l2Node!.val
+            if needCarry {
+                newVal += 1
+            }
+            if newVal >= 10 {
+                newVal = newVal % 10
+                needCarry = true
+            } else {
+                needCarry = false
+            }
+            
+            let newNode = ListNode(newVal)
+            resultList.next = newNode
+            l1Node = l1Node?.next
+            l2Node = l2Node?.next
+            resultList = resultList.next!
+        }
+        if l1Node != nil {
+            repeat {
+                var newVal = l1Node!.val
+                if needCarry {
+                    newVal += 1
+                }
+                
+                if newVal > 10 {
+                    newVal = newVal % 10
+                    needCarry = true
+                } else {
+                    needCarry = false
+                }
+                
+                resultList.next = l1Node
+                l1Node = l1Node?.next
+            } while l1Node != nil
+        }
+        if l2Node != nil {
+            repeat {
+                var newVal = l2Node!.val
+                if needCarry {
+                    newVal += 1
+                }
+                
+                if newVal > 10 {
+                    newVal = newVal % 10
+                    needCarry = true
+                } else {
+                    needCarry = false
+                }
+                
+                resultList.next = l2Node
+                l2Node = l1Node?.next
+            } while l2Node != nil
+        }
+        return resultListNode.next
+    }
+}
--- AlgorithmDemo/main.swift
+++ AlgorithmDemo/main.swift
@@ -12,3 +12,57 @@ print("Hello, World!")
 
 let sol = Solution()
 print(sol.twoSum([3,2,4], 6))
+
+class TestNode {
+    var next: TestNode?
+}
+
+class Test {
+    
+    static func test1() {
+        var node1: TestNode?
+        var node2: TestNode?
+        while true {
+            let newNode = TestNode()
+            
+            if node1 == nil {
+                node1 = newNode
+                node2 = node1
+            } else {
+                node1?.next = newNode
+            }
+            node1 = node1?.next
+            
+            print("")
+        }
+        
+    }
+    
+    static func test() {
+        
+//        let param_1 = ListNode(0)
+//        let param_2 = ListNode(0)
+        
+        let param_1 = ListNode(2)
+        let param_2 = ListNode(5)
+
+        param_1.next = ListNode(4)
+        param_1.next?.next = ListNode(3)
+
+        param_2.next = ListNode(6)
+        param_2.next?.next = ListNode(4)
+
+        let ret = Solution().addTwoNumbers(param_1, param_2)
+        var resultNode = sol.addTwoNumbers(param_1, param_2) //[2,4,3] [5,6,4]
+        while resultNode != nil {
+            print(resultNode?.val ?? 0)
+            resultNode = resultNode?.next
+        }
+    }
+}
+
+Test.test()
+Test.test1()
+
+
+
