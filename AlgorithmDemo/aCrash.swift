96a6447c1c47ccd165c1a1e70dcf74d4e41348c9
王嘉宁 <wangjianing@imdada.cn>
2020-04-28 10:08:55 +0800

    a crash

---
+++ AlgorithmDemo/aCrash.swift
@@ -0,0 +1,15 @@
+//
+//  aCrash.swift
+//  AlgorithmDemo
+//
+//  Created by Johnny on 2020/4/26.
+//  Copyright © 2020 Johnny. All rights reserved.
+//
+
+import Foundation
+
+//func aCrash(param: [String: AnyClass]) {
+//
+//}
+
+//aCrash(param: ["id":String.Type])
